018b6aba7b28cb1ffa493708e0e1420ef0c87e71
Alecay <83975131+Alecay@users.noreply.github.com>
2025-03-18 12:11:24 -0500

    Removed Old Nav comps

--- portfolio/src/components/bootstrap/navbar/NavbarDropdownDivider.tsx
+++
@@ -1,7 +0,0 @@
-export default function NavbarDropdownDivider() {
-  return (
-    <li>
-      <hr className="dropdown-divider" />
-    </li>
-  );
-}
--- portfolio/src/components/bootstrap/navbar/NavbarItem.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-interface Props {
-  children?: ReactNode;
-}
-
-export default function NavbarItem({ children }: Props) {
-  return <li className="nav-item">{children}</li>;
-}
